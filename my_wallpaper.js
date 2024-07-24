5718df7596fa998d32f6018f4ee0921093555996
jaime-robertson <robertjaim1@myvuw.ac.nz>
2024-07-25 11:18:05 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -3,17 +3,19 @@
 //let rect_height = 20;
 
 
+
 function setup_wallpaper(pWallpaper) {
   pWallpaper.output_mode(DEVELOP_GLYPH);
   pWallpaper.resolution(FIT_TO_SCREEN);
   pWallpaper.show_guide(true); //set this to false when you're ready to print
 
   //Grid settings
-  pWallpaper.grid_settings.cell_width  = 100;
-  pWallpaper.grid_settings.cell_height = 100;
+  pWallpaper.grid_settings.cell_width  = 200;
+  pWallpaper.grid_settings.cell_height = 200;
   pWallpaper.grid_settings.row_offset  = 50;
 
-  angleMode(degrees);
+  angleMode(DEGREES);
+
 }
 
 function wallpaper_background() {
@@ -23,23 +25,30 @@ function wallpaper_background() {
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
   //rect(40 ,40, rect_width, rect_height);
   
-  noStroke();
+  strokeWeight(0.4);
 
   fill(196, 159, 207); //light purple 
-  ellipse(50,30,50,10);
-  triangle(40,40,50,0,60,40)
+  ellipse(100,59,100,17);
+  triangle(80,80,100,0,120,80)
 
   fill(133, 109, 140); //darker purple
-  ellipse(50,29,50,3)
+  ellipse(100,58,100,6)
 
-  fill(123, 201, 144); //green
-  ellipse(50,45,30,25);
-  rect(37,50,27,25);
-
-  ellipse(40,35,10,10)
-  ellipse(60,35,10,10)
-
+  strokeWeight(0.4);
   
+  fill(123, 201, 144); //green
+  rect(75,95,50,50);
+  ellipse(80,70,20,20);
+  ellipse(120,70,20,20);
 
+  ellipse(100,85,60,40);
+
+  rect(75,145,10,30)
+  
+  fill(0); //black
+  ellipse(80,70,7,7);
+  ellipse(120,70,7,7);
+
+ 
   
 }
