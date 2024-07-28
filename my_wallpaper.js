a000df72b73a6593cf6f1b686ae4ec6302645130
jaime-robertson <robertjaim1@myvuw.ac.nz>
2024-07-28 17:55:14 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -2,7 +2,11 @@
 //let rect_width  = 20;
 //let rect_height = 20;
 
-let eyeSize = 9
+let eyeSize = 7
+
+let rectX = 0
+let rectY = 0
+
 
 function setup_wallpaper(pWallpaper) {
   pWallpaper.output_mode(DEVELOP_GLYPH);
@@ -12,14 +16,14 @@ function setup_wallpaper(pWallpaper) {
   //Grid settings
   pWallpaper.grid_settings.cell_width  = 200;
   pWallpaper.grid_settings.cell_height = 200;
-  pWallpaper.grid_settings.row_offset  = 50;
+  pWallpaper.grid_settings.row_offset  = 0;
 
   angleMode(DEGREES);
 
 }
 
 function wallpaper_background() {
-  background(240, 255, 240); //light honeydew green colour
+  background(57, 60, 64); // dark grey
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
@@ -28,28 +32,75 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
   strokeWeight(0.2);
 
   fill(196, 159, 207); //light purple 
-  ellipse(100,66,100,17);
-  triangle(80,80,100,0,120,80)
+  ellipse(100,80,100,13);
+  triangle(80,80,100,30,120,80)
 
   fill(133, 109, 140); //darker purple
-  ellipse(100,64,98,6)
+  ellipse(100,79,98,4)
 
   strokeWeight(0.2);
   
   fill(123, 201, 144); //green
-  rect(75,95,50,50);
-  ellipse(80,68,20,20);
-  ellipse(120,68,20,20);
+  rect(85,105,30,50);
+  ellipse(80,80,14,14);
+  ellipse(120,80,14,14);
 
-  ellipse(100,85,60,40);
+  ellipse(100,95,50,30);
 
-  rect(75,145,10,30)
-  rect(115,145,10,30)
+  rect(75,155,10,30)
+  rect(115,155,10,30)
   
   fill(0); //black
-  ellipse(78,65,eyeSize,eyeSize);
-  ellipse(122,65,eyeSize,eyeSize);
+  ellipse(80,77,eyeSize,eyeSize);
+  ellipse(120,77,eyeSize,eyeSize);
 
- 
+
+// checkered border
+  fill(0); //black
+  rect(rectX,rectY,20,20);
+  rect(rectX,rectY+40,20,20);
+  rect(rectX,rectY+80,20,20);
+  rect(rectX,rectY+120,20,20);
+  rect(rectX,rectY+160,20,20);
+  rect(rectX,rectY+200,20,20);
+
+  fill(123, 201, 144); //green
+  rect(rectX,rectY+20,20,20);
+  rect(rectX,rectY+60,20,20);
+  rect(rectX,rectY+100,20,20);
+  rect(rectX,rectY+140,20,20);
+  rect(rectX,rectY+180,20,20);
+
+  fill(123, 201, 144); //green
+  rect(rectX+20,rectY,20,20);
+  rect(rectX+60,rectY,20,20);
+  rect(rectX+100,rectY,20,20);
+  rect(rectX+140,rectY,20,20);
+  rect(rectX+180,rectY,20,20);
+
+  fill(0); //black
+  rect(rectX+40,rectY,20,20);
+  rect(rectX+80,rectY,20,20);
+  rect(rectX+120,rectY,20,20);
+  rect(rectX+160,rectY,20,20);
+
+  fill(123, 201, 144); //green
+  rect(rectX+180,rectY+40,20,20);
+  rect(rectX+180,rectY+80,20,20);
+  rect(rectX+180,rectY+120,20,20);
+  rect(rectX+180,rectY+160,20,20);
+
+  fill(0); //black
+  rect(rectX+180,rectY+20,20,20);
+  rect(rectX+180,rectY+60,20,20);
+  rect(rectX+180,rectY+100,20,20);
+  rect(rectX+180,rectY+140,20,20);
+  rect(rectX+180,rectY+180,20,20);
+
+  fill(123, 201, 144); //green
+  //rect(rectX+40,rectY+200);
+
+  fill(0); //black
+  //rect(rectX+20,rectY+200);  
   
 }
